514a6494eb249289359d169420f2128042883ce3
ppp <974270397@qq.com>
2019-07-25 11:13:28 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Button } from 'antd';
+import * as React from 'react';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <Button type="primary">Primary</Button>
+        <Button>Default</Button>
+        <Button type="dashed">Dashed</Button>
+        <Button type="danger">Danger</Button>
+        <Button type="link">Link</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
