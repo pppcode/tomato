28014b3e87f3ebbc5f8ca3fda72fea36c52b3526
ppp <974270397@qq.com>
2019-07-25 15:20:25 +0800

    配置 axios

---
+++ src/config/axios.ts
@@ -0,0 +1,38 @@
+import axios from 'axios'
+
+const appID = 'yB4EiD1Fn4iD6CSQAdrStd8F'
+const appSecret = 'f4GQD1qPMiC979ABK9ph6LYu'
+
+const instance = axios.create({
+  baseURL: 'https://gp-server.hunger-valley.com/',
+  headers: {
+    't-app-id': appID,
+    't-app-secret': appSecret
+  }
+});
+
+// Add a request interceptor
+instance.interceptors.request.use(function (config) {
+  const xToken = localStorage.getItem('x-token')
+  if(xToken) {
+    config.headers['Authorization'] = `Bearer ${xToken}`
+  }
+  return config;
+}, function (error) {
+  console.error(error)
+  return Promise.reject(error);
+});
+
+// Add a response interceptor
+instance.interceptors.response.use(function (response) {
+  // Do something with response data
+  if(response.headers['x-token']) {
+    localStorage.setItem('x-token',response.headers['x-token'])
+  }
+  return response;
+}, function (error) {
+  // Do something with response error
+  return Promise.reject(error);
+});
+
+export default instance
