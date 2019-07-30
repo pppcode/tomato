464237349b6719d1a1fc00759c0e9d75061ce797
ppp <974270397@qq.com>
2019-07-30 14:42:54 +0800

    添加 addTodo 到 TodoInput 组件

--- src/components/Todos/TodoInput.tsx
+++ src/components/Todos/TodoInput.tsx
@@ -1,4 +1,7 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
+import {addTodo} from "../../redux/actions";
+import axios from 'src/config/axios'
 import {Input, Icon} from 'antd';
 
 
@@ -7,7 +10,7 @@ interface ITodoInputState {
 }
 
 interface ITodoInputProps {
-  addTodo: (params:any) => void;
+  addTodo: (payload:any) => any;
 }
 
 class TodoInput extends React.Component<ITodoInputProps,ITodoInputState> {
@@ -21,19 +24,24 @@ class TodoInput extends React.Component<ITodoInputProps,ITodoInputState> {
 
   onKeyUp = (e)=>{
     if(e.keyCode === 13 && this.state.description !== '') {
-      this.addTodo()
+      this.postTodo()
       this.setState({description:''})
     }
   }
 
-  addTodo = () => {
-    this.props.addTodo({description: this.state.description})
+  postTodo = async ()=> {
+    try{
+      const response = await axios.post('todos', {description: this.state.description})
+      this.props.addTodo(response.data.resource)
+    }catch (e) {
+      throw new Error(e)
+    }
     this.setState({description:''})
   }
 
   public render() {
     const {description} = this.state;
-    const suffix = description ? <Icon type="enter" onClick={this.addTodo}/> : <span/>
+    const suffix = description ? <Icon type="enter" onClick={this.postTodo}/> : <span/>
     return (
       <Input
         placeholder="添加新任务"
@@ -46,5 +54,13 @@ class TodoInput extends React.Component<ITodoInputProps,ITodoInputState> {
   }
 }
 
-export default TodoInput
+const mapStateToProps = (state, ownProps) => ({
+  ...ownProps
+})
+
+const mapDispatchToProps = {
+  addTodo
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(TodoInput)
 
